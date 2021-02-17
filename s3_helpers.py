a0bfaf71c443a73d1f117f2e04943060f1d033a7
Tony Kang <tsk9537@gmail.com>
2021-02-17 15:27:41 -0600

    Adding .gitignore

--- s3_helpers.py
+++ s3_helpers.py
@@ -336,37 +336,3 @@ def get_control_client_from_vpc():
     )
 
     return control_client
-
-
-def get_secret():
-    secret_name = "MySecretName"
-    region_name = "us-west-2"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name,
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceNotFoundException':
-            print("The requested secret " + secret_name + " was not found")
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            print("The request was invalid due to:", e)
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            print("The request had invalid params:", e)
-    else:
-        # Secrets Manager decrypts the secret value using the associated KMS CMK
-        # Depending on whether the secret was a string or binary, only one of these fields will be populated
-        if 'SecretString' in get_secret_value_response:
-            text_secret_data = get_secret_value_response['SecretString']
-        else:
-            binary_secret_data = get_secret_value_response['SecretBinary']
-
-        # Your code goes here.
-
-
